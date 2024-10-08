97e368cc72052df3dae325fe74b2c63e1e6f5ced
gbprg <duarte.gabrielgbd@gmail.com>
2024-10-08 13:41:29 -0300

    navigation responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,6 +13,7 @@ export function Header() {
   return (
     <div className="w-full bg-primary">
       <div className="mx-auto flex min-h-20 max-w-[1200px] items-center justify-between p-6">
+        {/* Logo Section */}
         <div className="flex items-center gap-2">
           <CakeSlice className="size-8 text-white" />
           <h1 className="text-2xl font-bold text-white">Velvet</h1>
